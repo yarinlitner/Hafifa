c90328af9f762c4efe1b440430851e1d07aa98dc
Yarin Litner <ylitner@uwaterloo.ca>
2026-08-17 16:03:14 +0300

    update

--- threading_multiprocessing/writers.py
+++ threading_multiprocessing/writers.py
@@ -5,10 +5,10 @@ import pandas as pd
 from typing import Protocol, List
 
 class StockDataWriter(Protocol):
-    def write(self, stock_data_list: List[StockData], filepath: str) -> None:
+    def write(self, stock_data_list: List[StockData], file_path: str) -> None:
         ...
 
 class CsvStockDataWriter:
-    def write(self, stock_data_list: List[StockData], filepath: str) -> None:
+    def write(self, stock_data_list: List[StockData], file_path: str) -> None:
         df = pd.DataFrame(stock_data_list)
-        df.to_csv(filepath, index=False)
+        df.to_csv(file_path, index=False)
